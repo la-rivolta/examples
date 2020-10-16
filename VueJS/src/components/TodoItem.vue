b2e7f469770c84578667414251883fba4831292a
n.bilai <n.bilai@rt-solar.ru>
2020-10-16 23:15:37 +0300

    test folders vue

---
+++ VueJS/src/components/TodoItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <li>
+    <span :class="{done: todo.completed}">
+      <input type="checkbox" @change="todo.completed = !todo.completed">
+      <strong>{{ index + 1 }}</strong>
+      {{ todo.title | uppercase }}
+    </span>
+    <button class="delete" @click="$emit('remove-todo', todo.id)">&times</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+    index: Number
+  },
+  filters: {
+    uppercase(value){
+     return value.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style scoped>
+  li{
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: space-between;
+    padding: .5rem 2rem;
+    margin-bottom: 1rem;
+  }
+
+  .delete{
+    background-color: red;
+    border-radius: 40%;
+    color: white;
+    border: none;
+  }
+
+  input{
+    margin-right: 10px;
+  }
+  .done {
+    text-decoration: line-through;
+  }
+
+</style>
---
+++ VueJS/src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <router-link class="link" to="/todos">Go to todolist</router-link>
+  </div>
+</template>
